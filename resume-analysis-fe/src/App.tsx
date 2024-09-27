3fc8521f923cf1843ff932c5ed0daf55ca5c67e0
Phankawee Chulakasian <phankchu@nm7260831.metrosystems.co.th>
2024-09-27 20:47:36 +0700

    fix conflicted

---
+++ resume-analysis-fe/src/App.tsx
@@ -0,0 +1,14 @@
+import DragDropFileUpload from './Component/DragDropFileUpload';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DragDropFileUpload/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
